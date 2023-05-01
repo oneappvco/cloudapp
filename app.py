8abb1952c2af820c6d7178952eb59ff177ae4d3a
oneappvco <131608848+oneappvco@users.noreply.github.com>
2023-05-01 20:54:42 +0330

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+def test():
+    showinfo("Cloudapp","Hello Cloudapp")
+
+
+root=Tk()
+root.title("Cloudapp demo")
+root.geometry("250x250")
+root.resizable(False,False)
+
+Label(root,text="\n").pack()
+Label(root,text="Welcome To CloudAPP Demo").pack()
+Label(root,text="\n").pack()
+Button(root,text="Click Me",command=test).pack()
+
+root.mainloop()
